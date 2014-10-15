d585ef3feb4435896d5ca661f2bdb66c2b605166
Johnny88 <jpangs88@gmail.com>
2014-10-15 17:35:09 -0400

    initial commit of the home page

---
+++ client/components/carousel/carousel.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('sendtApp')
+  .controller('CarouselCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+      var newWidth = 600 + slides.length;
+      slides.push({
+        image: 'http://placehold.it/900x500',
+        text: 'placeholder'
+      });
+    };
+    for (var i=0; i<4; i++) {
+      $scope.addSlide();
+    }
+  });
