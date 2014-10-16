1e4a4e4e7b90bfb46678a6725f4fc783e2e474ca
mrgreek21 <andreaskyprianou@noam-imac.local>
2014-10-16 14:57:13 -0400

    Settings page should be working now

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,11 +4,20 @@ angular.module('sendtApp')
   .controller('MainCtrl', function ($scope, $http, socket, Auth) {
     $scope.awesomeThings = [];
     $scope.isLoggedIn = Auth.isLoggedIn;
-
+    $scope.inSettings = false;
+    
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
+
+    $scope.settings = function() {
+      $scope.inSettings = true;
+    };
+
+    $scope.closeSettings = function() {
+      $scope.inSettings = false;
+    };
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
